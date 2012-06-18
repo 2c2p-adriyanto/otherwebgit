d85cd6cc0274a2905f983aa3b6816b6b10691981
2c2p-adriyanto <adriyanto@sinaptiq.com>
2012-06-18 10:39:18 +0700

    komit pertama

---
+++ src/SolutionOther/BLOther/BLSomething.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLOther
+{
+    public class BLSomething
+    {
+        public string Hallo()
+        {
+            return "Hallo";
+        }
+    }
+}
